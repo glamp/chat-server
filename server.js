fae6b23431f15d0e54ef0c55d90b9fe825ce448e
glamp <lamp.greg@gmail.com>
2016-02-13 14:28:47 -0500

    porting

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
+var port = parseInt(proces.env.PORT || "3000");
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
